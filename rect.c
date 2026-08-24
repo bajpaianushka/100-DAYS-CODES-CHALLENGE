fa9c3d426a2f506f9897d08994667383263416c1
bajpaianushka <anushka.39934@stu.upes.ac.in>
2026-08-24 10:43:38 +0530

    Add files via upload

--- rect.c
+++ rect.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
-	int length = 3;
-	int  breadth = 7;
+	int length = 5;
+	int  breadth = 10;
 	scanf("%d %d", &length, &breadth);
 	int area = length * breadth;
 	int perimeter = 2 * (length + breadth);
